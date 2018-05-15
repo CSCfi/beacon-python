ce34fa0b2c04eddc48a5e27d3358296dec505356
Kasper Keinänen <kasper.keinanen@aalto.fi>
2018-05-15 14:05:32 +0300

    files transfered to github

---
+++ beacon_flask_api.py
@@ -0,0 +1,144 @@
+from flask import Flask, jsonify, request, abort
+from flask_restful import reqparse, Api, Resource
+from beacon_dicts import Beacon, BeaconAllelRequest, BeaconDataset
+from webargs import fields, validate, missing
+from webargs.flaskparser import use_kwargs, parser
+
+
+app = Flask(__name__)
+api = Api(app)
+
+refname = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y']
+start_list = [1, 2]
+refbases = ['A', 'C', 'G', 'T', 'N']
+assembly_list =['GRCh37', 'GRCh38']
+datasetIds_list = []
+for set in BeaconDataset:
+    datasetIds_list.append(set['id'])
+datasetresponses = ['ALL', 'HIT', 'MISS', 'NONE', None]
+
+class Beacon_get(Resource):
+    def get(self):
+        return jsonify(Beacon)#Not the same order as in example
+
+api.add_resource(Beacon_get,'/')
+
+
+class Beacon_query(Resource):
+    args = {
+        'referenceName': fields.Str(
+            missing='0'
+            #required=True,
+        ),
+        'start': fields.Int(
+            missing=0
+            #required=True,
+        ),
+        'startMin': fields.Int(
+            missing=0
+        ),
+        'startMax': fields.Int(
+            missing=0
+        ),
+        'end': fields.Int(
+            missing=0
+        ),
+        'endMin': fields.Int(
+            missing=0
+        ),
+        'endMax': fields.Int(
+            missing=0
+        ),
+        'referenceBases': fields.Str(
+            missing='0'
+            #required=True,
+        ),
+        'alternateBases': fields.Str(
+            missing='0'
+            #required=True,
+        ),
+        'assemblyId': fields.Str(
+            missing='0'
+            #required=True,
+        ),
+        'datasetIds': fields.List(fields.Str(
+            missing=None
+        )),
+        'includeDatasetResponses': fields.Str(
+            missing=None,
+        ),
+
+    }
+
+    @use_kwargs(args)
+    def get(self, referenceName, start, startMin, startMax, end, endMin, endMax, referenceBases, alternateBases, assemblyId, datasetIds, includeDatasetResponses):
+        datasetAllelResponses = ''
+
+        if datasetIds:
+            if datasetIds[0] not in datasetIds_list:
+                abort(400, 'Bad request (e.g. missing mandatory parameter)')
+        else:
+            datasetIds = None
+
+        if referenceName == '0' or start == 0 or referenceBases == '0' or alternateBases == '0' or assemblyId == '0':
+            abort(400, 'Bad request (e.g. missing mandatory parameter)')
+
+        if referenceName not in refname or start not in start_list or referenceBases not in refbases or assemblyId not in assembly_list or includeDatasetResponses not in datasetresponses:
+            abort(400, 'Bad request (e.g. missing mandatory parameter)')
+
+        if includeDatasetResponses == None:
+            includeDatasetResponses = False
+            datasetAllelResponses = None
+
+
+        allelRequest = {'referenceName': referenceName,
+                        'start': start,
+                        #'startMin': startMin,
+                        #'startMax': startMax,
+                        #'end': end,
+                        #'endMin': endMin,
+                        #'endMax': endMax,
+                        'referenceBases': referenceBases,
+                        'alternateBases': alternateBases,
+                        'assemblyId': assemblyId,
+                        'datasetIds': datasetIds,
+                        'includeDatasetResponses': includeDatasetResponses,
+                        }
+
+        return {'beaconId': 'ega-beacon',
+                'exists': False,
+                'error': None,
+                'allelRequest': allelRequest,
+                'datasetAllelResponses': datasetAllelResponses}
+
+    @use_kwargs(args)
+    def post(self, referenceName, start, startMin, startMax, end, endMin, endMax, referenceBases, alternateBases, assemblyId, datasetIds , includeDatasetResponses):
+
+        allelRequest = {'referenceName': referenceName,
+                        'start': start,
+                        # 'startMin': startMin,
+                        # 'startMax': startMax,
+                        # 'end': end,
+                        # 'endMin': endMin,
+                        # 'endMax': endMax,
+                        'referenceBases': referenceBases,
+                        'alternateBases': alternateBases,
+                        'assemblyId': assemblyId,
+                        'datasetIds': datasetIds,
+                        'includeDatasetResponses': includeDatasetResponses,
+                        }
+
+        return {"beaconId": "ega-beacon",
+                "exists": False,
+                "error": None,
+                "alleleRequest": allelRequest,
+                "datasetAlleleResponses": None}
+
+api.add_resource(Beacon_query,'/query')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
