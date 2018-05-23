8012d4d521a117f24531492a7fb906d8e953aa1c
Kasper Keinänen <kasper.keinanen@aalto.fi>
2018-05-23 10:51:13 +0300

    test database added to project with test data

---
+++ beacon_database.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/kakeinan/beacon-python/example.db'
+db = SQLAlchemy(app)
+
+class Beacon(db.Model):
+    __tablename__ = 'EGAD00000000028'
+    id = db.Column('id', db.String,primary_key=True)
+    dataset_id = db.Column('dataset_id', db.String)
+    start = db.Column('start', db.String)
+    chromosome = db.Column('chromosome', db.String)
+    reference = db.Column('reference', db.String)
+    alternate = db.Column('alternate', db.String)
+    end = db.Column('end', db.String)
+    type = db.Column('type', db.String)
+    sv_length = db.Column('sv_length', db.String)
+    variant_cnt = db.Column('variant_cnt', db.String)
+    call_cnt = db.Column('call_cnt', db.String)
+    sample_cnt = db.Column('sample_cnt', db.String)
+    frequency = db.Column('frequency', db.String)
+
+
+    def __init__(self, id, dataset_id, start, chromosome, reference, alternate, end, type, sv_length, variant_cnt, call_cnt, sample_cnt, frequency):
+        self.id = id
+        self.dataset_id = dataset_id
+        self.start = start
+        self.chromosome = chromosome
+        self.reference = reference
+        self.alternate = alternate
+        self.end = end
+        self.type = type
+        self.sv_length = sv_length
+        self.variant_cnt = variant_cnt
+        self.call_cnt = call_cnt
+        self.sample_cnt = sample_cnt
+        self.frequency = frequency
+
+#file = open('EGAD00000000028.SNPs.txt', 'r')
+#i = 1
+#while file:
+#    str = file.readline().rstrip()
+#
+#    print(str)
+#    dataset_id, start, chromosome, reference, alternate, end, type, sv_length, variant_cnt, call_cnt, sample_cnt, frequency = str.split(";")
+#    add_new = Beacon(i, dataset_id, start, chromosome, reference, alternate, end, type, sv_length, variant_cnt, call_cnt, sample_cnt, frequency)
+#    db.session.add(add_new)
+#    db.session.commit()
+#    i += 1
