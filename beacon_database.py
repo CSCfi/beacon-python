8863ee0a10f75342cb2bccc24c2dcde98efef5dd
Kasper Keinänen <kasper.keinanen@aalto.fi>
2018-05-24 10:24:42 +0300

    added variable lenght

--- beacon_database.py
+++ beacon_database.py
@@ -7,31 +7,29 @@ db = SQLAlchemy(app)
 
 class Beacon(db.Model):
     __tablename__ = 'EGAD00000000028'
-    id = db.Column('id', db.String,primary_key=True)
-    dataset_id = db.Column('dataset_id', db.String)
-    start = db.Column('start', db.String)
-    chromosome = db.Column('chromosome', db.String)
-    reference = db.Column('reference', db.String)
-    alternate = db.Column('alternate', db.String)
-    end = db.Column('end', db.String)
-    type = db.Column('type', db.String)
-    sv_length = db.Column('sv_length', db.String)
-    variant_cnt = db.Column('variant_cnt', db.String)
-    call_cnt = db.Column('call_cnt', db.String)
-    sample_cnt = db.Column('sample_cnt', db.String)
-    frequency = db.Column('frequency', db.String)
+    id = db.Column(db.String(10),primary_key=True)
+    dataset_id = db.Column(db.String(10))
+    start = db.Column(db.String(50))
+    chromosome = db.Column(db.String(50))
+    reference = db.Column(db.String(50))
+    alternate = db.Column(db.String(50))
+    end = db.Column(db.String(50))
+    type = db.Column(db.String(50))
+    sv_length = db.Column(db.String(50))
+    variant_cnt = db.Column(db.String(50))
+    call_cnt = db.Column(db.String(50))
+    sample_cnt = db.Column(db.String(50))
+    frequency = db.Column(db.String(50))
 
 
-
-file = open('EGAD00000000028.SNPs.txt', 'r')
-i = 1
-while file:
-    str = file.readline().rstrip()
-
-    print(str)
-    dataset_id, start, chromosome, reference, alternate, end, type, sv_length, variant_cnt, call_cnt, sample_cnt, frequency = str.split(";")
-    add_new = Beacon(id=i, dataset_id=dataset_id, start=start, chromosome=chromosome, reference=reference, alternate=alternate, \
-                     end=end, type=type, sv_length=sv_length, variant_cnt=variant_cnt, call_cnt=call_cnt, sample_cnt=sample_cnt, frequency=frequency)
-    db.session.add(add_new)
-    db.session.commit()
-    i += 1
+def load_data():
+    file = open('EGAD00000000028.SNPs.txt', 'r')
+    i = 1
+    while file:
+        str = file.readline().rstrip()
+        dataset_id, start, chromosome, reference, alternate, end, type, sv_length, variant_cnt, call_cnt, sample_cnt, frequency = str.split(";")
+        add_new = Beacon(id=i, dataset_id=dataset_id, start=start, chromosome=chromosome, reference=reference, alternate=alternate, \
+                         end=end, type=type, sv_length=sv_length, variant_cnt=variant_cnt, call_cnt=call_cnt, sample_cnt=sample_cnt, frequency=frequency)
+        db.session.add(add_new)
+        db.session.commit()
+        i += 1
