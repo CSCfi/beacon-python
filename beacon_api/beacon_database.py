77a0e59b52997e28c422088e90682c951f4d3ba1
Kasper Keinänen <kasper.keinanen@aalto.fi>
2018-06-26 10:37:48 +0300

    optimized the load_data_table() function to write faster

--- beacon_api/beacon_database.py
+++ beacon_api/beacon_database.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import csv
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/kakeinan/beacon-python/beacon_api/example.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
 
 class Beacon_dataset_table(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,22 +37,57 @@ class Beacon_data_table(db.Model):
     sample_cnt = db.Column(db.Integer)
     frequency = db.Column(db.Integer)
 
+
+
 def load_dataset_table():
     add_new = Beacon_dataset_table(stable_id='EGAD00000000028',description= 'Sample variants',access_type= 'PUBLIC',reference_genome= 'grch37',variant_cnt= '74',call_cnt= '74',sample_cnt= '1')
     db.session.add(add_new)
     db.session.commit()
 
+def chunks(data, n=10000):
+    buffer = [None] * n
+    idx = 0
+    for record in data:
+        buffer[idx] = record
+        idx += 1
+        if idx == n:
+            yield buffer
+            buffer = [None] * n
+            idx = 0
+    if idx > 0:
+        yield buffer[:idx]
 
-def load_data_table():
-    file = open('EGAD00000000028.SNPs.txt', 'r')
+
+def load_data_table(filename):
+    rows = 0
+    csvData = csv.reader(open('./beacon_api/{}'.format(filename), "r"), delimiter=";")
     dataset = Beacon_dataset_table.query.filter_by(id=1).first()
-    while file:
-        str = file.readline().rstrip()
-        dataset_id, start, chromosome, reference, alternate, end, type, sv_length, variant_cnt, call_cnt, sample_cnt, frequency = str.split(";")
-        add_new = Beacon_data_table(dataset=dataset, start=start, chromosome=chromosome, reference=reference, alternate=alternate, \
-                         end=end, type=type, sv_length=sv_length, variant_cnt=variant_cnt, call_cnt=call_cnt, sample_cnt=sample_cnt, frequency=frequency)
+    divData = chunks(csvData)  # divide into 10000 rows each
+
+    for chunk in divData:
+        for dataset_id, start, chromosome, reference, alternate, end, type, sv_length, variant_cnt, call_cnt, sample_cnt, frequency in chunk:
+            add_new = Beacon_data_table(dataset=dataset, start=start, chromosome=chromosome, reference=reference,
+                                        alternate=alternate, \
+                                        end=end, type=type, sv_length=sv_length, variant_cnt=variant_cnt, call_cnt=call_cnt, sample_cnt=sample_cnt, frequency=frequency)
         db.session.add(add_new)
         db.session.commit()
+        rows += 10000
+        print(rows)
+
+#def load_data_table(filename):
+#    file = open('./beacon_api/{}'.format(filename), 'r')
+#    dataset = Beacon_dataset_table.query.filter_by(id=1).first()
+#    i = 0
+#    while file:
+#        str = file.readline().rstrip()
+#        dataset_id, start, chromosome, reference, alternate, end, type, sv_length, variant_cnt, call_cnt, sample_cnt, frequency = str.split(";")
+#        add_new = Beacon_data_table(dataset=dataset, start=start, chromosome=chromosome, reference=reference, alternate=alternate, \
+#                         end=end, type=type, sv_length=sv_length, variant_cnt=variant_cnt, call_cnt=call_cnt, sample_cnt=sample_cnt, frequency=frequency)
+#        db.session.add(add_new)
+#        db.session.commit()
+#        i += 1
+#        if i % 1000 == 0:
+#            print(i)
 
 def constructor():
 
