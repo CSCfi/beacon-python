81c2d8d21a6dcfe164cbf45eee0b99be77a20b1b
Kasper Keinänen <kasper.keinanen@aalto.fi>
2018-08-06 15:56:05 +0300

    adding theam

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/Users/kakeinan/beacon-python/beacon_api'))
+sys.path.insert(0, os.path.abspath('beacon-python/docs/'))
 
 
 # -- Project information -----------------------------------------------------
