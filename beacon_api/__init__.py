13bb8df06d183eb5e8ccfb2b6a7f5c1dd2070098
Teemu <kataja.teemu@gmail.com>
2018-11-20 11:55:17 +0000

    #32 configure beacon info from config file

--- beacon_api/__init__.py
+++ beacon_api/__init__.py
@@ -2,30 +2,34 @@
 
 . note:: In this file is where one would register information about the ``Beacon``.
 """
+import os
 import datetime
 
+from .conf.config import parse_configuration_file
 
-__title__ = 'EGA Beacon'
-__version__ = VERSION = '1.0.0'
-__author__ = 'CSC developers'
-__license__ = 'Apache 2.0'
-__copyright__ = 'CSC - IT Center for Science'
+CONFIG = parse_configuration_file(os.environ.get('CONFIG_FILE', './beacon_api/conf/config.ini'))
+
+__title__ = CONFIG.title
+__version__ = CONFIG.version
+__author__ = CONFIG.author
+__license__ = CONFIG.license
+__copyright__ = CONFIG.copyright
 
 
-__apiVersion__ = "1.0.0"
-__beaconId__ = "ega-beacon"
-__description__ = f'Beacon API Web Server based on the GA4GH Beacon API {__apiVersion__}'
-__url__ = 'https://ega-archive.org/'
-__alturl__ = 'https://ega-archive.org/beacon/#/'
-__createtime__ = '2018-07-25T00:00:00Z'  # Fixed date, based when the beacon was first created
+__apiVersion__ = CONFIG.apiVersion
+__beaconId__ = CONFIG.beaconId
+__description__ = CONFIG.description
+__url__ = CONFIG.url
+__alturl__ = CONFIG.alturl
+__createtime__ = CONFIG.createtime
 __updatetime__ = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')  # Every restart of the application means an update to it
 
 
-__org_id__ = 'CSC'
-__org_name__ = __copyright__
-__org_description__ = """CSC – Finnish expertise in ICT for research, education, culture and public administration."""
-__org_address__ = 'empty'
-__org_welcomeUrl__ = 'https://www.csc.fi/'
-__org_contactUrl__ = 'empty'
-__org_logoUrl__ = 'https://www.csc.fi/documents/10180/161914/CSC_2012_LOGO_RGB_72dpi.jpg'
-__org_info__ = [{'author': __author__}]
+__org_id__ = CONFIG.org_id
+__org_name__ = CONFIG.org_name
+__org_description__ = CONFIG.org_description
+__org_address__ = CONFIG.org_address
+__org_welcomeUrl__ = CONFIG.org_welcomeUrl
+__org_contactUrl__ = CONFIG.org_contactUrl
+__org_logoUrl__ = CONFIG.org_logoUrl
+__org_info__ = [{'orgInfo': CONFIG.org_info}]
