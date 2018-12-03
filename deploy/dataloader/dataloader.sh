be72426095628d7340fbbb91ef7b1d758850d258
Stefan Negru <stefan@blankdots.com>
2018-12-03 08:52:13 +0200

    timeout for connection and dataloader env variables

--- deploy/dataloader/dataloader.sh
+++ deploy/dataloader/dataloader.sh
@@ -2,11 +2,12 @@
 
 LOCALDIR="/app/data"
 REGEX="ALL\.chr.*\.vcf\.gz$"
-REMOTEDIR="/vol1/ftp/release/20130502/"
+FTP=${FTP_URL="ftp://ftp.1000genomes.ebi.ac.uk"}
+REMOTEDIR=${FTP_DIR:="/vol1/ftp/release/20130502/"}
 
 cd $LOCALDIR
 ls -al
-lftp ftp://ftp.1000genomes.ebi.ac.uk << EOF
+lftp $FTP << EOF
     cd $REMOTEDIR
     mirror --only-missing -v -r -P 3 -i $REGEX
 EOF
