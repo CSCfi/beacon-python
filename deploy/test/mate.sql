d7cd78aab9a4d9b5e2aa4f52130772cd25e66558
Teemu Kataja <teemu.kataja@csc.fi>
2019-05-09 07:39:04 +0300

    add mate.sql to docker-compose start-phase and remove whitespaces

--- deploy/test/mate.sql
+++ deploy/test/mate.sql
@@ -1,3 +1,3 @@
 insert into beacon_mate_table values
-  (1, 'urn:hg:1000genome', 2 , 321681, 'bnd_W',  13, 123460, 'bnd_X', 'G'  , 'G'  , 0, 0, 0, 123460),
-  (2, 'urn:hg:1000genome', 13, 123460, 'bnd_X',   2, 321681, 'bnd_W', 'A'  , 'A'  , 0, 0, 0, 321681);
+  (1, 'urn:hg:1000genome', 2, 321681, 'bnd_W',  13, 123460, 'bnd_X', 'G', 'G', 0, 0, 0, 123460),
+  (2, 'urn:hg:1000genome', 13, 123460, 'bnd_X',   2, 321681, 'bnd_W', 'A', 'A', 0, 0, 0, 321681);
