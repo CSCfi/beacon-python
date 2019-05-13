572a165a11ff3efe0265e92e690f244731fc8d23
MalinAhlberg <ahlberg.malin@gmail.com>
2019-05-13 11:16:24 +0200

    Add missing whitespace

--- beacon_api/api/info.py
+++ beacon_api/api/info.py
@@ -9,7 +9,7 @@ and their associated metadata.
 from .. import __apiVersion__, __title__, __version__, __description__, __url__, __alturl__, __handover_beacon__
 from .. import __createtime__, __updatetime__, __org_id__, __org_name__, __org_description__
 from .. import __org_address__, __org_logoUrl__, __org_welcomeUrl__, __org_info__, __org_contactUrl__
-from .. import  __sample_queries__,__handover_drs__
+from .. import  __sample_queries__, __handover_drs__
 from ..utils.data_query import fetch_dataset_metadata
 from ..extensions.handover import make_handover
 from aiocache import cached
