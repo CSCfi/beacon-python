0167c111554f11203508bcad236dcd65fdfb7da6
Stefan Negru <blankdots@users.noreply.github.com>
2019-05-13 07:33:40 +0300

    fix typo for comment in integration tests

--- deploy/test/run_tests.py
+++ deploy/test/run_tests.py
@@ -8,7 +8,7 @@ async def main():
     """Run the tests."""
     LOG.debug('Start integration tests')
     # tests 18, 19 and 20 are also tested in the unit tests
-    # redundant, but later we may want to valdiate against json schema
+    # redundant, but later we may want to validate against JSON schema
     all_functions = inspect.getmembers(tests, inspect.isfunction)
     for x in all_functions:
         await x[1]()
